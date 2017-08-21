4b1db8e6131d9c1b75ebf2ea936b0b23e3f596b1
Chiibi <Chiibi_jame@hotmail.com>
2017-08-21 12:18:31 +0700

    [Add] Lab02 workshop
    - View & Layout workshop

---
+++ lab02/MyApplication/app/src/main/java/lab/mobile/myapplication/MyApplication.java
@@ -0,0 +1,13 @@
+package lab.mobile.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyApplication extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.relative_layout);
+    }
+}
